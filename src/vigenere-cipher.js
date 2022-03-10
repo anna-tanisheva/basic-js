e63836b80fb5b9779a7ed67cc9c3127dec3a198f
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-03-10 13:01:47 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,105 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  constructor () {
-    
+  constructor(direct) {
+    this.direct = direct;
+    if (arguments.length === 0) {
+      this.direct = true
+    }
   }
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key || arguments.length !== 2) {
+      throw new Error(`Incorrect arguments!`)
+    } else {
+      function isUpperCase(letter) {
+        let l = letter.charCodeAt();
+        if (l >= 65 && l <= 90) {
+          return true;
+        } else {
+          return false;
+        }
+      };
+      if (!this.direct) {
+        message = message.split('').reverse().join('').toUpperCase();
+        key = key.split('').reverse().join('').toUpperCase();
+        let cipherText = ``;
+        for (let i = 0, j = 0; i < message.length; i++) {
+          let currentLetter = message[i];
+
+          if (isUpperCase(currentLetter)) {
+            cipherText += String.fromCharCode((((currentLetter.charCodeAt() - 65) + (key[j].charCodeAt() - 65)) % 26) + 65)
+            // j++;
+            j = ++j % key.length
+          } else {
+            cipherText += currentLetter;
+          }
+        }
+        return cipherText;
+      } else {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let cipherText = ``;
+        for (let i = 0, j = 0; i < message.length; i++) {
+          let currentLetter = message[i];
+          if (isUpperCase(currentLetter)) {
+            cipherText += String.fromCharCode((((currentLetter.charCodeAt() - 65) + (key[j].charCodeAt() - 65)) % 26) + 65)
+            // j++;
+            j = ++j % key.length
+          } else {
+            cipherText += currentLetter;
+          }
+        }
+        return cipherText;
+      }
+
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  decrypt(message, key) {
+    if (!message || !key || arguments.length !== 2) {
+      throw new Error(`Incorrect arguments!`)
+    } else {
+      function isUpperCase(letter) {
+        let l = letter.charCodeAt();
+        if (l >= 65 && l <= 90) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      if (!this.direct) {
+        message = message.split('').reverse().join('').toUpperCase();
+        key = key.split('').reverse().join('').toUpperCase();
+        let cipherText = ``;
+        for (let i = 0, j = 0; i < message.length; i++) {
+          let currentLetter = message[i];
+          if (isUpperCase(currentLetter)) {
+            cipherText += String.fromCharCode(90 - (25 - (currentLetter.charCodeAt() - key.charCodeAt(j))) % 26)
+            j = ++j % key.length
+          } else {
+            cipherText += currentLetter;
+          }
+
+        }
+        return cipherText;
+      } else {
+        message = message.toUpperCase()
+        key = key.toUpperCase()
+        let cipherText = ``;
+        for (let i = 0, j = 0; i < message.length; i++) {
+          let currentLetter = message[i];
+          if (isUpperCase(currentLetter)) {
+            cipherText += String.fromCharCode(90 - (25 - (currentLetter.charCodeAt() - key.charCodeAt(j))) % 26)
+            j = ++j % key.length
+          } else {
+            cipherText += currentLetter;
+          }
+
+        }
+        return cipherText;
+      }
+    }
   }
 }
 
